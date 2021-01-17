6feea9d3d692af589c19662ccf13840c874fb5f2
Patrick Pires <patrick.lourenco@globalhitss.com.br>
2021-01-17 12:58:57 -0300

    feat: criando menu de contexto para lista de alunos e possibilitando exclusao atraves desse menu

--- app/src/main/java/br/com/cadastrodealunos/ui/activities/ListaAlunosActivity.kt
+++ app/src/main/java/br/com/cadastrodealunos/ui/activities/ListaAlunosActivity.kt
@@ -1,25 +1,54 @@
 package br.com.cadastrodealunos.ui.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
 import android.widget.AdapterView
+import android.widget.AdapterView.AdapterContextMenuInfo
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 import br.com.cadastrodealunos.R
 import br.com.cadastrodealunos.dao.AlunoDAO
 import br.com.cadastrodealunos.models.Aluno
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
+
 class ListaAlunosActivity : AppCompatActivity() {
     private val TITULO_APPBAR = "Lista de Alunos"
     private val alunoDao = AlunoDAO()
+    private lateinit var adapter: ArrayAdapter<Aluno>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         title = TITULO_APPBAR
         setContentView(R.layout.activity_lista_alunos)
         configuraFabNovoAluno()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        configuraLista()
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.activity_lista_alunos_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val itemId = item.itemId
+
+        if (itemId == R.id.activity_lista_alunos_menu_remover) {
+            val menuInfo = item.menuInfo as AdapterContextMenuInfo
+            val aluno = adapter.getItem(menuInfo.position) as Aluno
+            alunoDao.deleta(aluno)
+            atualizaListView(adapter)
+        }
+
+        return super.onContextItemSelected(item)
     }
 
     private fun configuraFabNovoAluno() {
@@ -33,36 +62,19 @@ class ListaAlunosActivity : AppCompatActivity() {
         startActivity(Intent(this, FormularioAlunoActivity::class.java))
     }
 
-    override fun onResume() {
-        super.onResume()
-        configuraLista()
-    }
-
     private fun configuraLista() {
         val listaAlunos = findViewById<ListView>(R.id.listaAlunos)
-        val adapter = ArrayAdapter(
+        configuraAdapter(listaAlunos)
+        configuraOnItemClick(listaAlunos)
+        registerForContextMenu(listaAlunos)
+    }
+
+    private fun configuraAdapter(listaAlunos: ListView) {
+        adapter = ArrayAdapter(
                 this,
                 android.R.layout.simple_list_item_1,
                 alunoDao.todos())
-
         listaAlunos.adapter = adapter
-
-        configuraOnItemClick(listaAlunos)
-        configuraOnItemLongClick(listaAlunos, adapter)
-    }
-
-    private fun configuraOnItemLongClick(listaAlunos: ListView, adapter: ArrayAdapter<Aluno>) {
-        listaAlunos.onItemLongClickListener = AdapterView.OnItemLongClickListener { parent, _, position, _ ->
-            val aluno = parent.getItemAtPosition(position) as Aluno
-            alunoDao.deleta(aluno)
-            atualizaListView(adapter)
-            true
-        }
-    }
-
-    private fun atualizaListView(adapter: ArrayAdapter<Aluno>) {
-        adapter.clear()
-        adapter.addAll(alunoDao.todos())
     }
 
     private fun configuraOnItemClick(listaAlunos: ListView) {
@@ -72,5 +84,10 @@ class ListaAlunosActivity : AppCompatActivity() {
             intent.putExtra("aluno", aluno)
             startActivity(intent)
         }
+    }
+
+    private fun atualizaListView(adapter: ArrayAdapter<Aluno>) {
+        adapter.clear()
+        adapter.addAll(alunoDao.todos())
     }
 }
